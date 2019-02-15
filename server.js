7f6262cd957af59629f69452d0d53d9c6a2dd728
Samantha Helmbreck <samanthahelmbreck@Samanthas-MacBook-Air.local>
2019-02-15 09:17:18 -0800

    Committing results

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + "/views"));
+
+app.get('/index', function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get('/about', function(req, res) {
+    res.sendFile(__dirname + "/views/about.html");
+});
+
+app.get('/contact', function(req, res) {
+    res.sendFile(__dirname + "/views/contact.html");
+});
+
+app.get('/skills', function(req, res) {
+    res.sendFile(__dirname + "/views/skills.html");
+});
+
+app.listen(3000, function() {
+    console.log("You are listening to port 3000...")
+});
